7952ff2df9afcbfa3231bdf172c1c5dc25fba268
Sammy Libre <sammy007@users.noreply.github.com>
2014-07-29 20:48:20 +0600

    Extend !price with volumes

--- bot.rb
+++ bot.rb
@@ -28,13 +28,19 @@ def refresh_price
 
     if (@last_price_update.nil? || (Time.now - @last_price_update > 180))
       resp = RestClient.get("https://poloniex.com/public?command=returnTicker")
-      @price["Poloniex"] = JSON.parse(resp)["BTC_XMR"]["last"].to_f
+      @price["Poloniex"] = {}
+      @price["Poloniex"]["price"] = JSON.parse(resp)["BTC_XMR"]["last"].to_f.round(8)
+      @price["Poloniex"]["vol"] = JSON.parse(resp)["BTC_XMR"]["baseVolume"].to_f.round(2)
 
       resp = RestClient.get("http://api.hitbtc.com/api/1/public/XMRBTC/ticker")
-      @price["HitBTC"] = JSON.parse(resp)["last"].to_f
+      @price["HitBTC"] = {}
+      @price["HitBTC"]["price"] = JSON.parse(resp)["last"].to_f.round(8)
+      @price["HitBTC"]["vol"] = JSON.parse(resp)["volume"].to_f.round(2)
 
       resp = RestClient.get("https://api.mintpal.com/v1/market/stats/XMR/BTC")
-      @price["Mintpal"] = JSON.parse(resp)[0]["last_price"].to_f
+      @price["Mintpal"] = {}
+      @price["Mintpal"]["price"] = JSON.parse(resp)[0]["last_price"].to_f.round(8)
+      @price["Mintpal"]["vol"] = JSON.parse(resp)[0]["24hvol"].to_f.round(2)
 
       @last_price_update = Time.now
     end
@@ -103,16 +109,16 @@ bot = Cinch::Bot.new do
     next if silence?(m.channel)
 
     refresh_price
-    m.user.msg "1 XMR equals #{@price["Poloniex"].round(8)} BTC on Poloniex"
-    m.user.msg "1 XMR equals #{@price["HitBTC"].round(8)} BTC on HitBTC.com"
-    m.user.msg "1 XMR equals #{@price["Mintpal"].round(8)} BTC on Mintpal"
+    m.user.msg "Last: #{@price["Poloniex"]["price"]} BTC | Volume: #{@price["Poloniex"]["vol"]} | Poloniex | https://poloniex.com/exchange/btc_xmr"
+    m.user.msg "Last: #{@price["HitBTC"]["price"]} BTC | Volume: #{@price["HitBTC"]["vol"]} | HitBTC.com | https://hitbtc.com/terminal#XMRBTC"
+    m.user.msg "Last: #{@price["Mintpal"]["price"]} BTC | Volume: #{@price["Mintpal"]["vol"]} | Mintpal | https://www.mintpal.com/market/XMR/BTC"
   end
 
   on :message, /^!worth (\d+)/ do |m, amount|
     next if silence?(m.channel)
 
     refresh_price
-    total = (amount.to_f * @price["Poloniex"].to_f).round(8)
+    total = amount.to_f * @price["Poloniex"]["price"].to_f
     m.user.msg "#{amount} XMR = #{total} BTC"
   end
 
